94040d23aee5ec851b3606d00f35550cc06ad677
Filip Simko <flpmko@gmail.com>
2021-03-22 11:17:33 +0100

    init commit

---
+++ app/src/main/java/sk/uniza/fri/simko/cviko_4/MainActivity.kt
@@ -0,0 +1,49 @@
+package sk.uniza.fri.simko.cviko_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.lang.Math.random
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button)
+        btn.setOnClickListener { rollDice() }
+
+        rollDice()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceRoll = dice.roll()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
